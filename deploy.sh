d08610605aafd2d0d3bf77a98cd5f5e26675eab7
Adriaan van Rossum <1079135+adriaanvanrossum@users.noreply.github.com>
2022-06-30 13:20:32 +0200

    Run deploy command on server

--- deploy.sh
+++ deploy.sh
@@ -21,16 +21,16 @@ echo "=> Uploading server part via SSH"
 current_date=$(date +"%Y-%m-%d")
 scp -q -o LogLevel=QUIET app.zip "$SSH_SERVER:$SSH_FOLDER/$current_date-app.zip"
 
-# read -r -p "=> Are you sure you want to deploy to production? [y/N] " prompt
-# if ! [[ $prompt == "y" ]]; then
-#   rm -f app.zip
-#   echo "Aborted by you. Next time, type 'y' to confirm."
-#   exit 0
-# fi
+read -r -p "=> Are you sure you want to deploy to production? [y/N] " prompt
+if ! [[ $prompt == "y" ]]; then
+  rm -f app.zip
+  echo "Aborted by you. Next time, type 'y' to confirm."
+  exit 0
+fi
 
-# echo "=> Deploying"
-# echo
-# command="cd $SSH_FOLDER && unzip -q -o $current_date-app.zip && npm install --production && pm2 restart news-alerts"
-# ssh "$SSH_SERVER" 'bash -i -c "'$command'"'
+echo "=> Deploying"
+echo
+command="cd $SSH_FOLDER && unzip -q -o $current_date-app.zip && npm install --production && pm2 restart news-alerts"
+ssh "$SSH_SERVER" 'bash -i -c "'$command'"'
 
-# rm -f app.zip
+rm -f app.zip
