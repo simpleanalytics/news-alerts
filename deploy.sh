bcc6d7e8dea7b9cb8047a7ee3678c60f838c1bfd
Adriaan van Rossum <1079135+adriaanvanrossum@users.noreply.github.com>
2022-07-01 10:26:16 +0200

    Show commit message in Telegram after reboot

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-set -eum
+set -euo pipefail
 
 if [ -f .env ]; then
   export $(grep -v '^#' .env | sed 's/\r$//' | awk '/=/ {print $1}' )
@@ -10,9 +10,34 @@ else
   exit 1
 fi
 
+read -r -p "=> Are you sure you want to deploy to production? [y/N] " prompt
+if ! [[ $prompt == "y" ]]; then
+  rm -f app.zip
+  echo "Aborted by you. Next time, type 'y' to confirm."
+  exit 0
+fi
+
 echo "=> Building"
 
 rm -f app.zip
+
+repository=$(git remote -v | head -n1 | perl -lwne 'm{\b([^/:]+/[^/]+).git\s} and print $1')
+
+last_commit=$(git log \
+    -1 \
+    --date=iso8601-strict \
+    --pretty=format:"{%n  \"commit\": \"%H\",%n  \"author\": \"%aN <%aE>\",%n  \"date\": \"%ad\",%n  \"timestamp\": %at,%n  \"message\": \"%f\",%n  \"repo\": \"$repository\"%n}," \
+    "$@" | \
+    perl -pe 's/},/}/' | \
+    perl -pe 's{\\}{\\\\}g')
+
+if [ -f ./commits/current.json ]; then
+  mv ./commits/current.json ./commits/previous.json
+  echo "$last_commit" > ./commits/current.json
+else
+  echo "$last_commit" > ./commits/current.json
+  echo "{}" > ./commits/previous.json
+fi
 
 zip -r -q app.zip . -x node_modules/\* -x .\* -x \*.zip -x \*.db
 
@@ -20,13 +45,6 @@ echo "=> Uploading to server"
 
 current_date=$(date +"%Y-%m-%d")
 scp -q -o LogLevel=QUIET app.zip "$SSH_SERVER:$SSH_FOLDER/$current_date-app.zip"
-
-read -r -p "=> Are you sure you want to deploy to production? [y/N] " prompt
-if ! [[ $prompt == "y" ]]; then
-  rm -f app.zip
-  echo "Aborted by you. Next time, type 'y' to confirm."
-  exit 0
-fi
 
 echo "=> Deploying on server"
 echo
--- index.js
+++ index.js
@@ -2,15 +2,34 @@ require("dotenv").config();
 
 const crawlers = require("./crawlers/index");
 const notify = require("./lib/notify");
-const { ms } = require("@simpleanalytics/common");
+const { ms, logger } = require("@simpleanalytics/common");
 const { loop } = require("./lib/utils");
 const { sendmessage } = require("./lib/telegram");
 
 const { NODE_ENV = "development" } = process.env;
 
 (async () => {
-  if (NODE_ENV === "production")
-    sendmessage("News alerts app just started up.", { silent: true });
+  if (NODE_ENV === "production") {
+    let message = "News alerts app just started up";
+    try {
+      const current = require("./commits/current.json");
+      const previous = require("./commits/previous.json");
+
+      const url =
+        previous.commit && current.commit
+          ? `https://github.com/${current.repo}/compare/${previous.commit}...${current.commit}`
+          : null;
+
+      const commit = current?.message?.replace(/-/g, " ");
+      message = `Deployed "${commit}" ${
+        url ? `<a href="${url}">GitHub</a>` : ""
+      }`;
+    } catch (error) {
+      logger.error(error);
+    }
+
+    sendmessage(message, { silent: true });
+  }
 
   loop(crawlers, { interval: ms.second * 90 });
   loop(notify, { interval: ms.second * 30 });
